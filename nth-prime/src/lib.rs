d33c49bad9d731f5be847144a0086fb685e53783
psvansluis <psvansluis@users.noreply.github.com>
2023-07-23 18:03:52 +0200

    nth prime works

--- nth-prime/src/lib.rs
+++ nth-prime/src/lib.rs
@@ -1,3 +1,16 @@
 pub fn nth(n: u32) -> u32 {
-    unimplemented!("What is the 0-indexed {n}th prime number?")
+    let mut primes = vec![2, 3];
+    while primes.len() <= n as usize {
+        grow_primes_list(&mut primes, 2);
+    }
+    primes[n as usize]
+}
+
+fn grow_primes_list(primes: &mut Vec<u32>, offset: u32) {
+    let candidate = primes.last().unwrap() + offset;
+    if primes.iter().any(|prime| candidate % prime == 0) {
+        grow_primes_list(primes, offset + 2)
+    } else {
+        primes.push(candidate);
+    }
 }
