50c6e8153821c7c55ee437d9f8f8ece28ea8fab5
psvansluis <psvansluis@users.noreply.github.com>
2023-07-23 18:44:54 +0200

    refactor

--- nth-prime/src/lib.rs
+++ nth-prime/src/lib.rs
@@ -1,15 +1,20 @@
 pub fn nth(n: u32) -> u32 {
     let mut primes = vec![2, 3];
-    while primes.len() <= n as usize {
-        grow_primes_list(&mut primes, 2);
-    }
+    grow_primes_until(&mut primes, n as usize);
     primes[n as usize]
 }
 
+fn grow_primes_until(primes: &mut Vec<u32>, target_length: usize) {
+    if primes.len() <= target_length {
+        grow_primes_list(primes, 2);
+        grow_primes_until(primes, target_length);
+    }
+}
+
 fn grow_primes_list(primes: &mut Vec<u32>, offset: u32) {
-    let candidate = primes.last().unwrap() + offset;
+    let candidate: u32 = primes.last().unwrap() + offset;
     if primes.iter().any(|prime| candidate % prime == 0) {
-        grow_primes_list(primes, offset + 2)
+        grow_primes_list(primes, offset + 2);
     } else {
         primes.push(candidate);
     }
