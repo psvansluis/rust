6cacab54f38f4e2e697d360dfb893e24ee949224
psvansluis <psvansluis@users.noreply.github.com>
2023-08-02 21:04:44 +0200

    luhn

---
+++ luhn/src/lib.rs
@@ -0,0 +1,37 @@
+pub fn is_valid(code: &str) -> bool {
+    match code
+        .chars()
+        .filter(|ch| !ch.is_whitespace())
+        .map(|ch| ch.to_digit(10))
+        .collect::<Option<Vec<u32>>>()
+    {
+        Some(digits) if digits.len() > 1 => luhn_sum(&digits) % 10 == 0,
+        _ => false,
+    }
+}
+
+fn luhn_sum(digits: &[u32]) -> u32 {
+    match digits {
+        [] => 0,
+        [head, tail @ ..] => double_maybe(*head, tail.len()) + luhn_sum(tail),
+    }
+}
+
+fn double_maybe(digit: u32, tail_length: usize) -> u32 {
+    match digit {
+        d if tail_length % 2 < 1 => d,
+        d @ 5.. => (d * 2) - 9,
+        d => d * 2,
+    }
+}
+
+#[test]
+fn luhn_sum_works_valid() {
+    let v = vec![4, 5, 3, 9, 3, 1, 9, 5, 0, 3, 4, 3, 6, 4, 6, 7];
+    assert_eq!(luhn_sum(&v), 80);
+}
+#[test]
+fn luhn_sum_works_invalid() {
+    let v = vec![8, 2, 7, 3, 1, 2, 3, 2, 7, 3, 5, 2, 0, 5, 6, 9];
+    assert_eq!(luhn_sum(&v), 57);
+}
