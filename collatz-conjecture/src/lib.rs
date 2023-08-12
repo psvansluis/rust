6105eaeb60f7621af2b86cc33a79ee322514f7b8
psvansluis <psvansluis@users.noreply.github.com>
2023-08-12 17:54:33 +0200

    refactor collatz

--- collatz-conjecture/src/lib.rs
+++ collatz-conjecture/src/lib.rs
@@ -3,11 +3,11 @@ pub fn collatz(n: u64) -> Option<u64> {
 }
 
 fn collatz_next(n: u64, steps_taken: u64) -> Option<u64> {
-    match n {
-        0 => None,
-        1 => Some(steps_taken),
-        n if n % 2 == 0 => collatz_next(n / 2, steps_taken + 1),
-        _ => n
+    match (n, n % 2) {
+        (0, _) => None,
+        (1, _) => Some(steps_taken),
+        (_, 0) => collatz_next(n / 2, steps_taken + 1),
+        (_, _) => n
             .checked_mul(3)
             .and_then(|multiplied| multiplied.checked_add(1))
             .and_then(|added| collatz_next(added, steps_taken + 1)),
