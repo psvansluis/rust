e80ff1c623574b5d960f64ddb485624f81ab48ac
Paulus van Sluis <paulus.van.sluis@persgroep.net>
2023-08-25 16:51:27 +0200

    refactor

--- diamond/src/lib.rs
+++ diamond/src/lib.rs
@@ -1,20 +1,17 @@
 pub fn get_diamond(c: char) -> Vec<String> {
     let chars = ('A'..=c).collect::<Vec<char>>();
-    match chars.len() {
-        1 => vec!['A'.to_string()],
-        len => chars
-            .iter()
-            .chain(chars.iter().rev().skip(1))
-            .map(|ch| {
-                let dist_from_a: usize = *ch as usize - 'A' as usize;
-                let side = || " ".repeat(len - 1 - dist_from_a);
-                let mid = || " ".repeat((dist_from_a * 2) - 1);
-                match ch {
-                    'A' => vec![side(), side()],
-                    _ => vec![side(), mid(), side()],
-                }
-                .join(&ch.to_string())
-            })
-            .collect::<Vec<String>>(),
-    }
+    chars
+        .iter()
+        .chain(chars.iter().rev().skip(1))
+        .map(|ch| {
+            let dist_from_a: usize = *ch as usize - 'A' as usize;
+            let side = || " ".repeat(chars.len() - 1 - dist_from_a);
+            let mid = || " ".repeat((dist_from_a * 2) - 1);
+            match ch {
+                'A' => vec![side(), side()],
+                _ => vec![side(), mid(), side()],
+            }
+            .join(&ch.to_string())
+        })
+        .collect::<Vec<String>>()
 }
