7df4f26e31742c9c464834c0a3b19bc54f86b3bf
psvansluis <psvansluis@users.noreply.github.com>
2023-09-03 17:56:28 +0200

    works

--- palindrome-products/src/lib.rs
+++ palindrome-products/src/lib.rs
@@ -1,24 +1,29 @@
+const BASE: u64 = 10;
+
 /// `Palindrome` is a newtype which only exists when the contained value is a palindrome number in base ten.
 ///
 /// A struct with a single field which is used to constrain behavior like this is called a "newtype", and its use is
 /// often referred to as the "newtype pattern". This is a fairly common pattern in Rust.
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Ord, PartialOrd)]
 pub struct Palindrome(u64);
 
 impl Palindrome {
     /// Create a `Palindrome` only if `value` is in fact a palindrome when represented in base ten. Otherwise, `None`.
     pub fn new(value: u64) -> Option<Palindrome> {
-        match Self::is_palindrome(value, 0) {
+        match value % BASE > 0 && {
+            let (head, tail) = Self::head_and_tail(value, 0);
+            head == tail
+        } {
             true => Some(Self(value)),
             false => None,
         }
     }
 
-    fn is_palindrome(head: u64, tail: u64) -> bool {
+    fn head_and_tail(head: u64, tail: u64) -> (u64, u64) {
         if head > tail {
-            Self::is_palindrome(head / 10, tail * 10 + head % 10)
+            Self::head_and_tail(head / BASE, tail * BASE + head % BASE)
         } else {
-            head == tail || head == tail / 10
+            (head, tail / if tail > head * BASE { BASE } else { 1 })
         }
     }
 
@@ -29,7 +34,14 @@ impl Palindrome {
 }
 
 pub fn palindrome_products(min: u64, max: u64) -> Option<(Palindrome, Palindrome)> {
-    unimplemented!(
-        "returns the minimum and maximum number of palindromes of the products of two factors in the range {min} to {max}"
-    );
+    let mut palindromes: Vec<Palindrome> = (min..=max)
+        .map(|n| (n..=max).map(move |m| n * m))
+        .flatten()
+        .filter_map(Palindrome::new)
+        .collect();
+    palindromes.sort_unstable();
+    match palindromes.len() {
+        0 => None,
+        len => Some((palindromes[0], palindromes[len - 1])),
+    }
 }
