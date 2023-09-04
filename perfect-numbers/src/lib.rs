e4cf5e4731e026df670ced4284200f4bb888f547
Paulus van Sluis <paulus.van.sluis@persgroep.net>
2023-09-04 09:13:13 +0200

    perfect numbers

--- perfect-numbers/src/lib.rs
+++ perfect-numbers/src/lib.rs
@@ -6,5 +6,10 @@ pub enum Classification {
 }
 
 pub fn classify(num: u64) -> Option<Classification> {
-    unimplemented!("classify {num}");
+    match (1..=num / 2).filter(|m| num % m == 0).sum::<u64>() {
+        _ if num == 0 => None,
+        equal if equal == num => Some(Classification::Perfect),
+        less if less < num => Some(Classification::Deficient),
+        _more => Some(Classification::Abundant),
+    }
 }
