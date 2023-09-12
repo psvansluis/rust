c03f7fb944cbc05c612ec79c5c5f8e14e2d7cd46
Paulus van Sluis <paulus.van.sluis@persgroep.net>
2023-09-12 11:47:29 +0200

    protein translation

--- protein-translation/src/lib.rs
+++ protein-translation/src/lib.rs
@@ -1,21 +1,25 @@
-pub struct CodonsInfo<'a> {
-    // We fake using 'a here, so the compiler does not complain that
-    // "parameter `'a` is never used". Delete when no longer needed.
-    phantom: std::marker::PhantomData<&'a ()>,
-}
+use std::collections::HashMap;
+const STOP: &Option<&str> = &Some("stop codon");
+
+pub struct CodonsInfo<'a>(HashMap<&'a str, &'a str>);
 
 impl<'a> CodonsInfo<'a> {
     pub fn name_for(&self, codon: &str) -> Option<&'a str> {
-        unimplemented!(
-            "Return the protein name for a '{codon}' codon or None, if codon string is invalid"
-        );
+        self.0.get(codon).copied()
     }
 
     pub fn of_rna(&self, rna: &str) -> Option<Vec<&'a str>> {
-        unimplemented!("Return a list of protein names that correspond to the '{rna}' RNA string or None if the RNA string is invalid");
+        rna.as_bytes()
+            .chunks(3)
+            .map(std::str::from_utf8)
+            .map(Result::ok)
+            .map(|codon| codon.map(|c| self.name_for(c)))
+            .map(Option::flatten)
+            .take_while(|protein| protein != STOP)
+            .collect()
     }
 }
 
 pub fn parse<'a>(pairs: Vec<(&'a str, &'a str)>) -> CodonsInfo<'a> {
-    unimplemented!("Construct a new CodonsInfo struct from given pairs: {pairs:?}");
+    CodonsInfo(pairs.into_iter().collect())
 }
