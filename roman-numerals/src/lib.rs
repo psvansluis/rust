4e842d621035f44e89cac1d5dbe102e4c4b26ae2
Paulus van Sluis <paulus.van.sluis@persgroep.net>
2023-09-25 11:28:06 +0200

    reorder

--- roman-numerals/src/lib.rs
+++ roman-numerals/src/lib.rs
@@ -1,24 +1,21 @@
 use std::fmt::Display;
 
-fn primitive(n: u16) -> Option<char> {
-    match n {
-        1 => Some('I'),
-        5 => Some('V'),
-        10 => Some('X'),
-        50 => Some('L'),
-        100 => Some('C'),
-        500 => Some('D'),
-        1000 => Some('M'),
-        _ => None,
-    }
-}
 pub struct Roman(u16);
 
 impl Display for Roman {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         if self.0 == 0 {
             return write!(f, "");
-        } else if let Some(p) = primitive(self.0) {
+        } else if let Some(p) = match self.0 {
+            1 => Some('I'),
+            5 => Some('V'),
+            10 => Some('X'),
+            50 => Some('L'),
+            100 => Some('C'),
+            500 => Some('D'),
+            1000 => Some('M'),
+            _ => None,
+        } {
             return write!(f, "{p}");
         }
         let high: u16 = match self.0 {
@@ -32,9 +29,9 @@ impl Display for Roman {
         };
 
         let prefix: u16 = match self.0 {
-            900..=1000 | 400..=500 => 100,
-            90..=100 | 40..=50 => 10,
-            9 | 4 => 1,
+            400..=499 | 900..=999 => 100,
+            40..=49 | 90..=99 => 10,
+            4 | 9 => 1,
             _ => 0,
         };
 
