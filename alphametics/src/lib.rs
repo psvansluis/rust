da38edd9ced8631bdf011b73ab29fc0a9a403096
Paulus van Sluis <paulus.van.sluis@persgroep.net>
2023-11-30 12:07:18 +0100

    alphametics refactor

--- alphametics/src/lib.rs
+++ alphametics/src/lib.rs
@@ -8,37 +8,31 @@ use itertools::Itertools;
 type Solution = HashMap<char, u8>;
 
 pub fn solve(input: &str) -> Option<Solution> {
+    let characters: HashSet<char> = input.chars().filter(char::is_ascii_uppercase).collect();
+    let n_chars = characters.len();
+    if n_chars > 10 {
+        return None;
+    }
     let terms: Vec<&str> = input
         .split(&['=', '+'][..])
         .filter(|str| !str.is_empty())
         .map(&str::trim)
         .collect();
     let (lhs, [rhs]) = terms.split_at(terms.len() - 1) else {return None;};
-    let characters: HashSet<char> = input.chars().filter(char::is_ascii_uppercase).collect();
-    let n_chars = characters.len();
-    if n_chars > 10 {
-        return None;
-    }
+    let no_initial_zeros = |solution: &Solution| -> bool {
+        solution
+            .iter()
+            .all(|(ch, num)| num > &0 || !terms.iter().any(|&term| term.starts_with(*ch)))
+    };
+    let lhs_eq_rhs = |solution: &Solution| -> bool {
+        let to_num = |term: &&str| {
+            term.chars()
+                .fold(0, |acc, ch| acc * 10 + *solution.get(&ch).unwrap() as u64)
+        };
+        lhs.iter().map(to_num).sum::<u64>().eq(&to_num(rhs))
+    };
     (0_u8..10)
         .permutations(n_chars)
         .map(|permutation| zip(characters.clone(), permutation).collect::<Solution>())
-        .find(|solution| {
-            no_initial_zeros(solution, &terms)
-                && lhs
-                    .iter()
-                    .map(|term| term_to_number(term, solution))
-                    .sum::<u64>()
-                    .eq(&term_to_number(rhs, solution))
-        })
-}
-
-fn term_to_number(term: &str, solution: &Solution) -> u64 {
-    term.chars()
-        .fold(0, |acc, ch| acc * 10 + *solution.get(&ch).unwrap() as u64)
-}
-
-fn no_initial_zeros(solution: &Solution, terms: &[&str]) -> bool {
-    solution
-        .iter()
-        .all(|(ch, num)| num > &0 || !terms.iter().any(|&term| term.starts_with(*ch)))
+        .find(|solution: &Solution| no_initial_zeros(solution) && lhs_eq_rhs(solution))
 }
