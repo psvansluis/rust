93cd3b2ef927f94d35f89a359a4c9d50f6cee6e4
Paulus van Sluis <psvansluis@gmail.com>
2024-03-13 13:11:09 +0100

    allergies

---
+++ allergies/src/lib.rs
@@ -0,0 +1,51 @@
+use std::ops::BitAnd;
+
+pub struct Allergies(u32);
+
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
+pub enum Allergen {
+    Eggs = 1,
+    Peanuts = 2,
+    Shellfish = 4,
+    Strawberries = 8,
+    Tomatoes = 16,
+    Chocolate = 32,
+    Pollen = 64,
+    Cats = 128,
+}
+
+impl BitAnd<u32> for Allergen {
+    type Output = u32;
+
+    fn bitand(self, rhs: u32) -> Self::Output {
+        self as u32 & rhs
+    }
+}
+
+const ALLERGENS: [Allergen; 8] = [
+    Allergen::Eggs,
+    Allergen::Peanuts,
+    Allergen::Shellfish,
+    Allergen::Strawberries,
+    Allergen::Tomatoes,
+    Allergen::Chocolate,
+    Allergen::Pollen,
+    Allergen::Cats,
+];
+
+impl Allergies {
+    pub fn new(score: u32) -> Self {
+        Self(score)
+    }
+
+    pub fn is_allergic_to(&self, allergen: &Allergen) -> bool {
+        self.allergies().contains(allergen)
+    }
+
+    pub fn allergies(&self) -> Vec<Allergen> {
+        ALLERGENS
+            .into_iter()
+            .filter(|allergen| *allergen & self.0 > 0)
+            .collect()
+    }
+}
